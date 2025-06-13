557d6ad3eb3ab3d58ceac4c95fecc58d1e8b38af
larsen360 <larsen360@live.dk>
2025-06-14 00:58:10 +0200

    Delete Powersehell lib install.ps1

--- Powersehell lib install.ps1
+++
@@ -1,7 +0,0 @@
-$requirementsFile = "requirements.txt"
-if (Test-Path $requirementsFile) {
-    pip install -r $requirementsFile
-} else { 
-    pip install requests
-    pip install beautifulsoup4
-
