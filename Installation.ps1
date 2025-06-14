777ecc3afbd0d5778f3d80c3fd9b13e4d5e99eae
Emil Larsen <larsen360@live.dk>
2025-06-14 20:37:06 +0200

    .

--- Installation.ps1
+++ Installation.ps1
@@ -2,6 +2,11 @@ $requirementsFile = "requirements.txt"
 if (Test-Path $requirementsFile) {
     python -m pip install -r $requirementsFile
 } else {
-    python -m pip install requests
-    python -m pip install beautifulsoup4
+    py -m pip install requests
+    py -m pip install beautifulsoup4
+    py -m pip install selenium
+    py -m pip install undetected-chromedriver
+    py -m pip install pillow
+    py -m pip install setuptools
+    py -m pip install pytesseract
 }
